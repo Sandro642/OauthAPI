0d3bae2a3ce84d4da75bf563a33e5e32e6438224
Sandro642 <sandro33810@gmail.com>
2023-06-30 01:15:21 +0200

    Update RegisterYaml.java

--- src/main/java/fr/sandro642/github/yaml/RegisterYaml.java
+++ src/main/java/fr/sandro642/github/yaml/RegisterYaml.java
@@ -1,6 +1,8 @@
 package fr.sandro642.github.yaml;
 
+import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
@@ -10,6 +12,9 @@ public class RegisterYaml {
 
     private FileConfiguration configuration;
 
+    public static Configuration configyamldatastatus;
+    public static Configuration configyamlplayerdatasecret;
+
     private static Plugin plugin;
     public static void setPlugin(Plugin plugin) {
         RegisterYaml.setPlugin(plugin);
@@ -18,12 +23,16 @@ public class RegisterYaml {
     public static void registerYamlStatus() throws IOException {
         File file = new File(plugin.getDataFolder(), "datastatus.yml");
 
+        configyamldatastatus = YamlConfiguration.loadConfiguration(file);
+
         plugin.getDataFolder().mkdirs();
         file.createNewFile();
     }
 
     public static void registerYamlSecret() throws IOException {
         File file = new File(plugin.getDataFolder(), "playerdatasecret.yml");
+
+        configyamlplayerdatasecret = YamlConfiguration.loadConfiguration(file);
 
         plugin.getDataFolder().mkdirs();
         file.createNewFile();
